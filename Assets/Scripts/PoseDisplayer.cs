37b569cae052dd32d5f16eb58c3cfca54c669a63
kazumakano <kazumakano@outlook.jp>
2022-09-27 15:27:45 +0900

    display orientation

--- Assets/Scripts/PoseDisplayer.cs
+++ Assets/Scripts/PoseDisplayer.cs
@@ -5,10 +5,12 @@ using TMPro;
 public class PoseDisplayer : MonoBehaviour
 {
     [SerializeField] new Camera camera;
+    [SerializeField] TextMeshProUGUI oriText;
     [SerializeField] TextMeshProUGUI posText;
 
     void FixedUpdate()
     {
+        oriText.SetText($"α: {(camera.transform.eulerAngles.x + 180) % 360 - 180}\n" + $"β: {(camera.transform.eulerAngles.y + 180) % 360 - 180}\n" + $"γ: {(camera.transform.eulerAngles.z + 180) % 360 - 180}\n");
         posText.SetText($"x: {camera.transform.position.x}\n" + $"y: {camera.transform.position.y}\n" + $"z: {camera.transform.position.z}\n");
     }
 }
