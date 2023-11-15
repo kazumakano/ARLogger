60652baaba14383b274eaca93b2a5d2ae794143f
kazumakano <kazumakano@outlook.jp>
2023-11-16 00:35:23 +0900

    modify intent bug

--- Assets/Scripts/Intent.cs
+++ Assets/Scripts/Intent.cs
@@ -8,10 +8,10 @@ public static class Intent
     static readonly AndroidJavaObject ctx = curAct.Call<AndroidJavaObject>("getApplicationContext");
     static readonly string auth = ctx.Call<string>("getPackageName") + ".fileprovider";
     static readonly AndroidJavaClass fileProvider = new("android.support.v4.content.FileProvider");
-    static readonly AndroidJavaObject intent = new("android.content.Intent");
 
     public static void OpenFile(string file, string type)
     {
+        AndroidJavaObject intent = new("android.content.Intent");
         intent.Call<AndroidJavaObject>("addFlags", intent.GetStatic<int>("FLAG_GRANT_READ_URI_PERMISSION"));
         intent.Call<AndroidJavaObject>("setAction", intent.GetStatic<string>("ACTION_VIEW"));
         intent.Call<AndroidJavaObject>(
@@ -30,6 +30,7 @@ public static class Intent
             uris.Call<bool>("add", fileProvider.CallStatic<AndroidJavaObject>("getUriForFile", ctx, auth, new AndroidJavaObject("java.io.File", f)));
         }
 
+        AndroidJavaObject intent = new("android.content.Intent");
         intent.Call<AndroidJavaObject>("putExtra", intent.GetStatic<string>("EXTRA_STREAM"), uris);
         intent.Call<AndroidJavaObject>("setAction", intent.GetStatic<string>("ACTION_SEND_MULTIPLE"));
         intent.Call<AndroidJavaObject>("setType", type);
