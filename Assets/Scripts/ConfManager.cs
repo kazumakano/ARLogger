131d2a107ae3d8589fd126489fdcb789ccde0ef3
kazumakano <kazumakano@outlook.jp>
2023-12-15 18:55:24 +0900

    apply unix time format

--- Assets/Scripts/ConfManager.cs
+++ Assets/Scripts/ConfManager.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class ConfManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField udpDestHostnameInputField;
     [SerializeField] TMP_InputField udpDestPortInputField;
+    [SerializeField] Toggle unixToggle;
 
     void Start()
     {
         udpDestHostnameInputField.text = PlayerPrefs.GetString("UDP Dest Hostname");
         udpDestPortInputField.text = PlayerPrefs.GetInt("UDP Dest Port", -1) >= 0 ? PlayerPrefs.GetInt("UDP Dest Port").ToString() : null;
+        unixToggle.isOn = PlayerPrefs.GetInt("Use UNIX Time Format") > 0;
+    }
+
+    public void OnChangeUnixToggle()
+    {
+        PlayerPrefs.SetInt("Use UNIX Time Format", unixToggle.isOn ? 1 : 0);
     }
 
     public void OnEndEditUdpDestHostname()
--- Assets/Scripts/PoseWriter.cs
+++ Assets/Scripts/PoseWriter.cs
@@ -11,10 +11,13 @@ public class PoseWriter : MonoBehaviour
 
     [NonSerialized] public string fileName;
     [NonSerialized] public string metaInfo;
+    Func<string> getTsStr;
     StreamWriter writer;
 
     void Start()
     {
+        getTsStr = PlayerPrefs.GetInt("Use UNIX Time Format") > 0 ? () => (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds.ToString() : () => DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff");
+
         if (metaInfo != "")
         {
             using (writer = new StreamWriter(fileName + ".meta"))
@@ -31,7 +34,7 @@ public class PoseWriter : MonoBehaviour
 
     void Update()
     {
-        writer.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff")},{cam.transform.position.x},{cam.transform.position.y},{cam.transform.position.z},{cam.transform.rotation.x},{cam.transform.rotation.y},{cam.transform.rotation.z},{cam.transform.rotation.w}");
+        writer.WriteLine($"{getTsStr()},{cam.transform.position.x},{cam.transform.position.y},{cam.transform.position.z},{cam.transform.rotation.x},{cam.transform.rotation.y},{cam.transform.rotation.z},{cam.transform.rotation.w}");
     }
 
     void OnDestroy()
